5f22a434fa46f4f0b378d2812030d22c303ace85
JUANSBAQUEROS <juan.baquero@paymentsway.com.co>
2024-06-12 02:16:22 -0500

    STABLE - loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderService } from './services/loader/loader-service';
+import { LoaderComponent } from "./components/loader/loader/loader.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, LoaderComponent, CommonModule]
 })
 export class AppComponent {
   title = 'vanti-app';
+
+  isLoading: boolean = true;
+
+  constructor(private loaderService: LoaderService) {}
+
+  ngOnInit() {
+    this.loaderService.loading$.subscribe((loading: boolean) => {
+      setTimeout(() => {
+        this.isLoading = loading;
+      }, 3000);
+    });
+  }
 }
--- src/app/layouts/admin/admin.component.ts
+++ src/app/layouts/admin/admin.component.ts
@@ -2,20 +2,33 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 import { CommonModule } from '@angular/common';
+import { LoaderService } from '../../services/loader/loader-service';
+import { LoaderComponent } from "../../components/loader/loader/loader.component";
 
 @Component({
     selector: 'app-admin',
     standalone: true,
     templateUrl: './admin.component.html',
     styleUrl: './admin.component.scss',
-    imports: [RouterOutlet, CommonModule]
+    imports: [RouterOutlet, CommonModule, LoaderComponent]
 })
 export class AdminComponent {
   isDark: boolean = true;
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private loaderService: LoaderService
   ){}
+
+  isLoading: boolean = true;
+
+  ngOnInit() {
+    this.loaderService.loading$.subscribe((loading: boolean) => {
+      setTimeout(() => {
+        this.isLoading = loading;
+      }, 3000);
+    });
+  }
 
   toggleTheme() {
     this.isDark = !this.isDark;
