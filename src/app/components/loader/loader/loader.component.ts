e5c606e5a36c91898c736a41494a4d547d70c180
JUANSBAQUEROS <juan.baquero@paymentsway.com.co>
2024-06-12 02:45:11 -0500

    STABLE - add loader animation fade

--- src/app/components/loader/loader/loader.component.ts
+++ src/app/components/loader/loader/loader.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { trigger, transition, style, animate, state } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   standalone: true,
   imports: [],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.scss'
+  styleUrl: './loader.component.scss',
+  animations: [
+    trigger('fadeAnimation', [
+      state('visible', style({
+        opacity: 1
+      })),
+      state('hidden', style({
+        opacity: 0
+      })),
+      transition('visible => hidden', [
+        animate('0.5s')
+      ])
+    ])
+  ]
 })
-export class LoaderComponent {
 
+export class LoaderComponent implements OnInit {
+  loaderState: 'visible' | 'hidden' = 'visible';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.hideLoader();
+    }, 2000);
+  }
+
+  hideLoader() {
+    this.loaderState = 'hidden';
+  }
 }
